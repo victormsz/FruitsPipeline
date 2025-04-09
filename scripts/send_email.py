eb5f4a9643593a0d27e2672142ddf2d1a854ceb9
victormsz09 <victormsz09@yahoo.com.br>
2025-04-09 19:06:41 -0300

    commit final

--- scripts/send_email.py
+++ scripts/send_email.py
@@ -2,14 +2,16 @@ import os
 import smtplib
 from email.mime.text import MIMEText
 
-dest_email = os.getenv("DEST_EMAIL", "default@example.com")
+email = os.environ.get("EMAIL_AUTOR")
 
 msg = MIMEText("Pipeline executado com sucesso!")
 msg['Subject'] = 'CI/CD Notification'
 msg['From'] = 'victormsz09@yahoo.com.br'
-msg['To'] = dest_email
+msg['To'] = email
 
 with smtplib.SMTP('smtp.gmail.com', 587) as server:
     server.starttls()
     server.login(os.getenv("SMTP_USER"), os.getenv("SMTP_PASS"))
     server.send_message(msg)
+
+
